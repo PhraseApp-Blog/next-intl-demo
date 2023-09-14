ac5ca8a913eca8f2c25c862f3aa57d18590783e6
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-14 12:58:11 +0100

    feat: add story counter

--- app/page.tsx
+++ app/page.tsx
@@ -30,24 +30,27 @@ export default function Home() {
         </div>
 
         <div className="ml-4">
-          <div className="mb-1 text-sm font-light text-lime-900">
+          <div className="mb-1 flex items-baseline justify-between text-sm font-light text-lime-900">
             <span className="-ml-1 rounded-sm bg-lime-50 px-1">
               The grapevine
             </span>
+            <span className="inline-block rounded-sm bg-lime-50/40 px-1 text-xs">
+              3 stories
+            </span>
           </div>
-          <article className="mb-2 border-b-[1px] border-stone-200 pb-3">
+          <article className="mb-2 border-b-[0.5px] border-lime-300 pb-3">
             <h2 className="text-sm">
-              Montana court rules children have right to a healthy environment
-              in major blow to fossil fuels
+              🧺 Montana court rules children have right to a healthy
+              environment in major blow to fossil fuels
             </h2>
             <div className="flex items-baseline justify-between text-xs font-light text-stone-500">
               <p>by Euronews Green with AP</p>
               <p>2023/08/15</p>
             </div>
           </article>
-          <article className="mb-2 border-b-[1px] border-stone-200 pb-3">
+          <article className="mb-2 border-b-[0.5px] border-lime-300 pb-3">
             <h2 className="text-sm">
-              India slashes emissions rate by one-third in 14 years
+              🌳 India slashes emissions rate by one-third in 14 years
             </h2>
             <div className="flex items-baseline justify-between text-xs font-light text-stone-500">
               <p>by Euronews Green with Reuters</p>
@@ -56,7 +59,7 @@ export default function Home() {
           </article>
           <article className="">
             <h2 className="text-sm">
-              Locals fix their broken items for free at this repair cafe
+              🧑‍🔧 Locals fix their broken items for free at this repair cafe
             </h2>
             <div className="flex items-baseline justify-between text-xs font-light text-stone-500">
               <p>by Angela Symons</p>
