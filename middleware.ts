fbea767573105022af64985f90bcf43bc33a256c
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-15 10:45:34 +0100

    feat: add explicit default local prefix e.g. /en-US

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ export default createMiddleware({
   // A list of all locales that are supported
   locales: ["en-US", "ar-EG"],
 
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: "en-US",
+  localePrefix: "always",
 });
 
 export const config = {
