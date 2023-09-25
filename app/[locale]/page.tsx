86db72dbbcbd4635f15f608358b6aea9aa0a67ab
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-25 14:54:30 +0100

    feat: plurals

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -37,7 +37,7 @@ export default async function Home({
               {t("feed")}
             </span>
             <span className="inline-block rounded-sm bg-lime-50/40 px-1 text-xs">
-              {feed.length} stories
+              {t("feedCount", { count: feed.length })}
             </span>
           </div>
           {feed.map((article) => (
