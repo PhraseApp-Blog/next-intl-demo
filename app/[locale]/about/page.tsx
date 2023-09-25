0e6b24bf1f3fd532757b2563020aa35f17c56ca1
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-25 14:28:56 +0100

    feat: basic translations (again)

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,4 +1,5 @@
 import { Locale } from "@/app/types";
+import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function AboutPage({
@@ -7,10 +8,13 @@ export default function AboutPage({
   params: { locale: Locale };
 }) {
   unstable_setRequestLocale(locale);
+  const t = useTranslations("About");
 
   return (
     <main className="px-6 py-2">
-      <h1 className="mb-2 mt-2 text-3xl font-bold text-lime-900">About</h1>
+      <h1 className="mb-2 mt-2 text-3xl font-bold text-lime-900">
+        {t("title")}
+      </h1>
       <p className="mt-2 text-lg">
         This is a demo of <a href="https://nextjs.org/">Next.js</a> App Router
         localization with{" "}
