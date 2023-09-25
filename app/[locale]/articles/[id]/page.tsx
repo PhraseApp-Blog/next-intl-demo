3391aef90a6dd9eed70af1d70673da253310045d
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-25 19:18:44 +0100

    feat: splitting components out of async page

--- app/[locale]/articles/[id]/page.tsx
+++ app/[locale]/articles/[id]/page.tsx
@@ -1,6 +1,30 @@
 import { Article, Locale } from "@/app/types";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { getTranslator } from "next-intl/server";
+
+function Source({ url }: { url: string }) {
+  const t = useTranslations("ArticlePage");
+
+  return (
+    <p>
+      {t.rich("source", {
+        a: (chunks) => <a href={url}>{chunks}</a>,
+      })}
+    </p>
+  );
+}
+
+function SummaryCredit() {
+  const t = useTranslations("ArticlePage");
+
+  return (
+    <p>
+      {t.rich("summaryCredit", {
+        a: (chunks) => <a href="https://chat.openai.com">{chunks}</a>,
+      })}
+    </p>
+  );
+}
 
 export default async function ArticlePage({
   params: { id },
@@ -24,18 +48,13 @@ export default async function ArticlePage({
       <h1 className="mb-2 mt-2 text-3xl font-bold text-lime-900">{title}</h1>
       <div className="flex items-baseline justify-between">
         <div className="me-3 inline-block rounded-sm bg-lime-50/40 px-2 py-1 text-sm">
-          <p className="text-stone-600">by {author}</p>
+          <p className="text-stone-600">{t("author", { author })}</p>
           <p className="text-stone-500">
             {t("publishDate", { date: Date.parse(publishedAt) })}
           </p>
         </div>
         <div className="rounded-sm bg-lime-50/40 px-2 py-1 text-right text-sm">
-          <p>
-            source: <a href={sourceUrl}>euronews.green</a>
-          </p>{" "}
-          <p>
-            [summarized by <a href="chat.openai.com/">ChatGPT</a>]
-          </p>
+          <Source url={sourceUrl} /> <SummaryCredit />
         </div>
       </div>
       <p className="mt-2 text-lg">{body}</p>
