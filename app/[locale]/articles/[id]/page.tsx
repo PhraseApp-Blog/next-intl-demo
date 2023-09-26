be59658796b114864c54af624449120bb11270fe
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-09-26 11:18:54 +0100

    feat: splitting components out of async page

--- app/[locale]/articles/[id]/page.tsx
+++ app/[locale]/articles/[id]/page.tsx
@@ -1,44 +1,9 @@
 import { Article, Locale } from "@/app/types";
 import { useLocale, useTranslations } from "next-intl";
-import { getTranslator } from "next-intl/server";
 
-function Source({ url }: { url: string }) {
-  const t = useTranslations("ArticlePage");
-
-  return (
-    <p>
-      {t.rich("source", {
-        a: (chunks) => <a href={url}>{chunks}</a>,
-      })}
-    </p>
-  );
-}
-
-function SummaryCredit() {
-  const t = useTranslations("ArticlePage");
-
-  return (
-    <p>
-      {t.rich("summaryCredit", {
-        a: (chunks) => <a href="https://chat.openai.com">{chunks}</a>,
-      })}
-    </p>
-  );
-}
-
-export default async function ArticlePage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const article: Article = await fetch(`${process.env.API_URL}/${id}`, {
-    next: { revalidate: +process.env.DEFAULT_CACHE_DURATION_IN_SECONDS! },
-  }).then((res) => res.json());
-
-  // We can call useLocale() here because this is a dynamic page.
+function ArticlePage({ article }: { article: Article }) {
   const locale = useLocale() as Locale;
-  // We need to call getTranslator() here because this is an async page.
-  const t = await getTranslator(locale, "ArticlePage");
+  const t = useTranslations("ArticlePage");
 
   const { author, publishedAt, sourceUrl } = article;
   const { title, body } = article.translations[locale];
@@ -54,10 +19,31 @@ export default async function ArticlePage({
           </p>
         </div>
         <div className="rounded-sm bg-lime-50/40 px-2 py-1 text-right text-sm">
-          <Source url={sourceUrl} /> <SummaryCredit />
+          <p>
+            {t.rich("source", {
+              a: (chunks) => <a href={sourceUrl}>{chunks}</a>,
+            })}
+          </p>
+          <p>
+            {t.rich("summaryCredit", {
+              a: (chunks) => <a href="https://chat.openai.com">{chunks}</a>,
+            })}
+          </p>
         </div>
       </div>
       <p className="mt-2 text-lg">{body}</p>
     </main>
   );
+}
+
+export default async function ArticlePageAsync({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const article: Article = await fetch(`${process.env.API_URL}/${id}`, {
+    next: { revalidate: +process.env.DEFAULT_CACHE_DURATION_IN_SECONDS! },
+  }).then((res) => res.json());
+
+  return <ArticlePage article={article} />;
 }
