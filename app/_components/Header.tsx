50e37a53868f4b4d4564a14a14d32e65d5bf23f0
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-04-17 19:16:33 +0100

    feat: add localized links

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,5 @@
+import { Link } from "@/i18n.config";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 export default function Header() {
   const t = useTranslations("Header");
--- i18n.config.ts
+++ i18n.config.ts
@@ -1,2 +1,7 @@
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
+
 export const locales = ["en-us", "ar-eg"] as const;
 export type Locale = (typeof locales)[number];
+
+export const { Link, usePathname, useRouter } =
+  createSharedPathnamesNavigation({ locales });
