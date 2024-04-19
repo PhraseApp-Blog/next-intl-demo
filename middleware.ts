d0bee078bc81376e828f373191f1d5848d657f97
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-04-19 14:20:16 +0100

    refactor(middleware): update route matcher config

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,6 @@ export default createMiddleware({
 
 export const config = {
   matcher: [
-    "/",
     // Match all pathnames except for
     // - … if they start with `/api`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
