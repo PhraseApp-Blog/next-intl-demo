8e73f4c94701f53978c447be99ec9e61b27b977a
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-04-23 15:25:00 +0100

    feat: type-safe message keys

--- types.ts
+++ types.ts
@@ -1,13 +1,28 @@
+import enus from "./locales/en-us.json";
+
+type Messages = typeof enus;
+
+declare global {
+  interface IntlMessages extends Messages {}
+}
+
 export interface WeeklyWeatherRoot {
   weeklyWeather: WeeklyWeather[];
 }
 
 export interface WeeklyWeather {
   dateTime: string;
-  condition: string;
+  condition: Condition;
   conditionIcon: string;
   temperature: Temperature;
 }
+
+export type Condition =
+  | "sunny"
+  | "cloudy"
+  | "rainy"
+  | "thunderstorms"
+  | "showers";
 
 export interface Temperature {
   celsius: number;
